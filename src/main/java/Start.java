867208b8ca38b16133104e6d56a8b668e826a01f
Sms_Gamer <creepercannyon@gmail.com>
2019-11-30 14:45:05 -0800

    Update Start.java

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 public class Start {
     public static void main(String[] args) {
         Main.main(concat(new String[]{"--version", "mcp", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}"}, args));
-        PasteClient.INSTANCE.initialize();
     }
 
     public static <T> T[] concat(T[] first, T[] second) {
