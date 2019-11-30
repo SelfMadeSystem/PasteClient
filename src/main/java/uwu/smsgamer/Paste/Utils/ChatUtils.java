dcefdbf87100bacdc3180bfdb83240d50d1e44ee
Sms_Gamer <creepercannyon@gmail.com>
2019-11-30 14:58:01 -0800

    Util stuff

---
+++ src/main/java/uwu/smsgamer/Paste/Utils/ChatUtils.java
@@ -0,0 +1,13 @@
+package uwu.smsgamer.Paste.Utils;
+
+import net.minecraft.util.IChatComponent;
+import uwu.smsgamer.Paste.PasteClient;
+
+public class ChatUtils {
+
+    public static final String colorSymbol = "§";
+
+    public static void chatMessage(String msg){
+        PasteClient.instance.mc.thePlayer.addChatMessage(IChatComponent.Serializer.jsonToComponent("{text:\"" + msg + "\"}"));
+    }
+}
